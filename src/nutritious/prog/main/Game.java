5f894ae9b3da623ebdf058658fda4c5ff676f9b6
nutristious-prog <artur.wlodarczyk12@gmail.com>
2022-08-01 18:29:07 +0200

    Adding longer level image and chaning LoadSave class to read longer levels

--- src/nutritious/prog/main/Game.java
+++ src/nutritious/prog/main/Game.java
@@ -20,7 +20,7 @@ public class Game implements Runnable {
 
     public final static int TILES_DEFAULT_SIZE = 32;
     public final static float SCALE = 1.5f;
-    public final static int TILES_IN_WIDTH = 26;
+    public final static int TILES_IN_WIDTH = 26;    // these two values is the visible with and height
     public final static int TILES_IN_HEIGHT = 14;
     public final static int TILES_SIZE = (int) (TILES_DEFAULT_SIZE * SCALE);
     public final static int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
--- src/nutritious/prog/utils/LoadSave.java
+++ src/nutritious/prog/utils/LoadSave.java
@@ -11,7 +11,8 @@ import java.io.InputStream;
 public class LoadSave {
     public static final String PLAYER_ATLAS = "player_sprites.png";
     public static final String LEVEL_ATLAS = "outside_sprites.png";
-    public static final String LEVEL_ONE_ATLAS = "level_one_data.png";
+//    public static final String LEVEL_ONE_ATLAS = "level_one_data.png";
+    public static final String LEVEL_ONE_ATLAS = "level_one_data_long.png";
     public static final String MENU_BUTTONS = "button_atlas.png";
     public static final String MENU_BACKGROUND = "menu_background.png";
     public static final String PAUSE_BACKGROUND = "pause_menu.png";
@@ -39,8 +40,8 @@ public class LoadSave {
     //displaying level as a two-dimensional array
     public static int[][] GetLevelData() {
         //initialising two-dimensional array for storing certain level tiles
-        int[][] levelData = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
         BufferedImage img = GetSpriteAtlas(LEVEL_ONE_ATLAS);
+        int[][] levelData = new int[img.getHeight()][img.getWidth()]; // the image is bigger than what we see on the screen
 
         //The size of the img will be the size of the lvl.
         //20 x 40 will make a lvl 20 tiles wide and 40 in height.
