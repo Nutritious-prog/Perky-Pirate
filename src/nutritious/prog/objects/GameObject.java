f915f8f5ef9931c288109f9c442be46ceaf6fb30
nutritious-prog <artur.wlodarczyk12@gmail.com>
2022-08-19 17:52:50 +0200

    Adding GameObject file
    Adding objects sprites

---
+++ src/nutritious/prog/objects/GameObject.java
@@ -0,0 +1,89 @@
+package nutritious.prog.objects;
+
+import nutritious.prog.main.Game;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
+import static nutritious.prog.utils.Constants.EnemyConstants.*;
+import static nutritious.prog.utils.Constants.EntityConstants.ANI_TIME;
+
+public class GameObject {
+    protected int x, y, objectType;
+    protected Rectangle2D.Float hitbox;
+    protected boolean playAnimation;                //potions will animate infinitely but barrels will animate only while destroyed
+    protected boolean active = true;
+    protected int animationTick, animationIndex;
+    protected int xDrawOffset, yDrawOffset;
+
+    public GameObject(int x, int y, int objectType) {
+        this.x = x;
+        this.y = y;
+        this.objectType = objectType;
+    }
+
+    protected void updateAnimationTick() {
+        animationTick++;
+        if(animationTick >= ANI_TIME) {
+            animationTick = 0;
+            animationIndex++;
+        }
+        if(animationIndex >= GetSpriteAmount(objectType)) {
+            animationIndex = 0;
+        }
+    }
+
+    public void reset() {
+        animationIndex = 0;
+        animationTick = 0;
+        active = true;
+
+        //TODO if statement for different types of objects
+        playAnimation = true;
+    }
+
+    protected void initHitbox(int width, int height) {
+        //we set hitbox to bo exact same as entity model
+        hitbox = new Rectangle2D.Float(x, y, (int)(width * Game.SCALE), (int)(height * Game.SCALE));
+    }
+
+    public void drawHitbox(Graphics graphics, int xLvlOffset) {
+        //for debugging hitbox
+        graphics.setColor(Color.RED);
+        graphics.drawRect((int)hitbox.x - xLvlOffset,(int)hitbox.y,(int) hitbox.width, (int)hitbox.height);
+    }
+
+    public int getObjectType() {
+        return objectType;
+    }
+
+    public void setObjectType(int objectType) {
+        this.objectType = objectType;
+    }
+
+    public Rectangle2D.Float getHitbox() {
+        return hitbox;
+    }
+
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean isActive) {
+        this.active = isActive;
+    }
+
+    public int getXDrawOffset() {
+        return xDrawOffset;
+    }
+
+    public int getYDrawOffset() {
+        return yDrawOffset;
+    }
+
+    public int getAnimationIndex() {
+        return animationIndex;
+    }
+
+}
--- src/nutritious/prog/utils/LoadSave.java
+++ src/nutritious/prog/utils/LoadSave.java
@@ -31,6 +31,8 @@ public class LoadSave {
     public static final String CRABBY_SPRITE = "crabby_sprite.png";
     public static final String STATUS_BAR = "health_power_bar.png";
     public static final String LEVEL_COMPLETED_BACKGROUND = "level_completed_background.png";
+    public static final String POTIONS_ATLAS = "potions_sprites.png";
+    public static final String DESTROYABLE_OBJECTS_ATLAS = "objects_sprites.png";
 
     public static BufferedImage GetSpriteAtlas(String fileName) {
         BufferedImage img = null;
